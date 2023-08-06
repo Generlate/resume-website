4a641f97914f0d28409700873e55394faf7383d3
Austen Cabret <austencabret@gmail.com>
2023-08-06 13:01:38 -0400

    fixing typescript errors

--- src/slideShow.ts
+++ src/slideShow.ts
@@ -11,8 +11,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
