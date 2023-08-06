ff843189cd768bcc7c604da599bf4bfce1fccb4c
Austen Cabret <austencabret@gmail.com>
2023-08-06 13:34:32 -0400

    debugging typescript

--- src/slideShow.ts
+++ src/slideShow.ts
@@ -1,26 +1,35 @@
-let slideIndex = 1;
+let slideIndex: number = 1;
 showSlides(slideIndex);
 
-function plusSlides(n) {
+function plusSlides(n: number) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
+function currentSlide(n: number) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlides(n: number) {
   let i;
-  const slides = document.getElementsByClassName("mySlides");
-  const dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  const slides: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+  const dots: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+
+  if (n > slides.length) {
+    slideIndex = 1;
   }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-} 
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
