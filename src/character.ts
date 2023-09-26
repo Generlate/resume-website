7ea2806251261871e799c7aa18d2025e034f6d40
Austen Cabret <austencabret@gmail.com>
2023-09-26 03:12:29 -0400

    debugging the loading screen

--- src/character.ts
+++ src/character.ts
@@ -35,7 +35,7 @@ function characterTransformation(glb: GLTF) {
 }
 
 function onProgress(xhr: any) {
-  const percentageLoaded = (xhr.loaded / xhr.total) * 100;
+  const percentageLoaded = (xhr.loaded / xhr.total) * 51;
   const loaderText: any = document.querySelector(".loader-text");
   loaderText.textContent = `${Math.round(percentageLoaded)}%`;
 }
