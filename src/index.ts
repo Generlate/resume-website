93a007cb95d7f2b889e8e4f85828a9131bcbb935
Austen Cabret <austencabret@gmail.com>
2023-11-18 23:44:53 -0500

    reorganized todo list

--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,12 @@ loadBuildingModel(scene);
 loadCharacterModel();
 createPortal(scene, camera, renderer, controls);
 
-// TODO: decide if i want to do media size flip or if text should scale with the browser window, add spacing between the sections on mobile
-// mobile needs bigger padding and margin, i think it should be static sizing. font size should be static.
-// make header modular
-// TODO: slideshow arrows change color on hover
-// TODO: slideshow arrow buttons should be same color as background and hover to different color
-// TODO: add link symbol to generlate.com website link
-// TODO: swap out technology names for icons that are greyed out and color on hover (links to the tech's site)
-// TODO: socials turn the socials color on hover
-// TODO: consider using inter font instead of roboto.. maybe, idk
-// TODO: maybe do 'Front End Software Engineer' instead
-// TODO: mess with the title's thickness
 // TODO: make outer fillet = inner fillet + padding
-// TODO: maybe section stroke changes on hover
-// TODO: fix projects not showing on mobile
-// TODO: maybe change slideshow to smaller project boxes?
+// TODO: mess with the title's thickness
+// TODO: consider using inter font instead of roboto.. maybe, idk
+// TODO: swap out technology names for icons that are greyed out and color on hover (links to the tech's site)
 // TODO: Add an all projects page like Brittany Chiang's
+// TODO: maybe change slideshow to smaller project boxes?
 // TODO: add technologies used for each project
 // TODO: break down technologies section to programming languages, scripting languages, tools, design, etc. (look at brittany chiang v.3)
 // TODO: add mongodb
@@ -34,5 +24,9 @@ createPortal(scene, camera, renderer, controls);
 // TODO: change icon to be different from generlate.com
 // TODO: network and add some social proof
 // TODO: add character click animation
-// TODO: make the proects work on the website instead of just linking to them
+// TODO: make the projects work on the website instead of just linking to them
 // TODO: transitions
+// TODO: slideshow arrows change color on hover
+// TODO: slideshow arrow buttons should be same color as background and hover to different color
+// TODO: add link symbol to generlate.com website link
+// TODO: fix projects not showing on mobile
