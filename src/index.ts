0b4de6123975c80ca44eee9fc7a3bd14ba4d7fd1
Austen Cabret <austencabret@gmail.com>
2023-12-04 17:58:20 -0500

    image resizing

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,18 @@ loadBuildingModel(scene);
 loadCharacterModel();
 createPortal(scene, camera, renderer, controls);
 
-// TODO: make shirt white
 // TODO: fix sub pages' mobile
-// TODO: fix broken environment on very skinny window
-// TODO: move character to the right to center on text
 // TODO: transitions
 // TODO: make transitions delayed
 // TODO: add animations to hover/click
 // TODO: better project descriptions (with code blocks?)
 // TODO: make generlate.com default light theme
+// TODO: fix broken environment on very skinny window
+// TODO: move character to the right to center on text
 // TODO: network and add some social proof
 // TODO: make cube animations use time instead of frames
 // TODO: add easter eggs
 // TODO: add character click animation
 // TODO: make the projects work on the website instead of just linking to them
 // TODO: add link symbol to generlate.com website link
+// TODO: make shirt white
