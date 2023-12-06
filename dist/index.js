e5aafc0f399888a8dab4d394e934e878444f9e19
Austen Cabret <austencabret@gmail.com>
2023-12-06 15:17:18 -0500

    changed loading aside's display to none

--- dist/index.js
+++ dist/index.js
@@ -7,24 +7,16 @@ setupScene(scene);
 loadBuildingModel(scene);
 loadCharacterModel();
 createPortal(scene, camera, renderer, controls);
-// TODO: make subpages mobile-first
-// TODO: maybe change slideshow to smaller project boxes?
-// TODO: Add an all projects page like Brittany Chiang's
-// TODO: add technologies used for each project
-// TODO: break down technologies section to programming languages, scripting languages, tools, design, etc. (look at brittany chiang v.3)
-// TODO: tech icons color on hover
-// TODO: make outer fillet = inner fillet + padding
-// TODO: add mongodb
-// TODO: account for the video loading in the loader or compress video
-// TODO: better project descriptions (with code blocks?)
-// TODO: add easter eggs
-// TODO: change icon to be different from generlate.com
-// TODO: network and add some social proof
+// TODO: fix header appearing before loading screen
+// TODO: page change animations
+// TODO: fix broken environment on very skinny window
+// TODO: move character to the right to center on text
+// TODO: make cube animations use time instead of frames
 // TODO: add character click animation
 // TODO: make the projects work on the website instead of just linking to them
-// TODO: transitions
-// TODO: slideshow arrows change color on hover
-// TODO: slideshow arrow buttons should be same color as background and hover to different color
 // TODO: add link symbol to generlate.com website link
-// TODO: fix projects not showing on mobile
+// TODO: make shirt white
+// TODO: move title to in the canvas (3D)
+// TODO: test on different browsers
+// TODO: clean up public folder
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,7 @@ createPortal(scene, camera, renderer, controls);
 // TODO: page change animations
 // TODO: fix broken environment on very skinny window
 // TODO: move character to the right to center on text
-// TODO: network and add some social proof
 // TODO: make cube animations use time instead of frames
-// TODO: add easter eggs
 // TODO: add character click animation
 // TODO: make the projects work on the website instead of just linking to them
 // TODO: add link symbol to generlate.com website link
