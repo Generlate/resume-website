b4c88727de55d264e563fc672d93b815e0ac8ec7
Austen Cabret <austencabret@gmail.com>
2024-03-02 20:39:31 -0500

    add 'playsinline' to building code consultant video

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import { scene, renderer, camera, controls } from './sceneInitialize.js';
-import { setupScene } from '../dist/sceneSetup.js';
-import { loadBuildingModel } from '../dist/building.js';
-import { loadCharacterModel } from '../dist/character.js';
-import { createPortal } from '../dist/portal.js';
+import { scene, renderer, camera, controls } from "./sceneInitialize.js";
+import { setupScene } from "../dist/sceneSetup.js";
+import { loadBuildingModel } from "../dist/building.js";
+import { loadCharacterModel } from "../dist/character.js";
+import { createPortal } from "../dist/portal.js";
 
 setupScene(scene);
 loadBuildingModel(scene);
 loadCharacterModel();
 createPortal(scene, camera, renderer, controls);
 
+// TODO: fix spacing from character to description section
+// TODO: make work on ipad and large screens
 // TODO: remove controls and add autoplay to home page videos on mobile
 // TODO: consider using clamp() and minmax() in CSS
 // TODO: try @media (orientation: landscape)
