71bf3b218d7471d164621398c08c8c610324a113
Austen Cabret <austencabret@gmail.com>
2024-03-21 18:57:32 -0400

    update react to next

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,14 @@ loadBuildingModel(scene);
 loadCharacterModel();
 createPortal(scene, camera, renderer, controls);
 
-// TODO: for the home page, move 2d into the 3d scene (simplifies complex scaling)
-// TODO: flip experience order in mobile
+// TODO: rebuild with StyleX
+// TODO: rebuild with next.js
+// TODO: clean up css
+// TODO: look in to using @media(prefers-color-scheme)
+// TODO: speed up through following lighthouse suggestions
 // TODO: consider using clamp() and minmax() in CSS
 // TODO: try @media (orientation: landscape)
-// TODO: make work on ipad and large screens
-// TODO: fix broken environment on very skinny window / landscape mobile
-// TODO: page change animations
+// TODO: page change animations (framer-motion)
 // TODO: reduce building.glb size to reduce load time
+// TODO: for the home page, move 2d into the 3d scene (simplifies complex scaling)
+// TODO: fix broken environment on very skinny window / landscape mobile
